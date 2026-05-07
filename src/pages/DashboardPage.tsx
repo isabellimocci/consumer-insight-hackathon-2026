c6ec3252e455455e8288e018cde2e1f468e0a18e
taisasoares <soares.taisa16@gmail.com>
2026-05-07 03:14:28 -0300

    fix: altera datas para padrao ISO-8601

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -8,8 +8,8 @@
 export default function DashboardPage() {
   //CHAMADA DE FUNÇÕES PARA TESTE
   // console.log('>>> getAllMonthsData', getAllMonthsData())
-  // const current = getTransactionsByMonth('04/2026')
-  // const previous = getTransactionsByMonth('03/2026')
+  // const current = getTransactionsByMonth('2026-04')
+  // const previous = getTransactionsByMonth('2026-03')
   // console.log(current)
   // console.log('>>> getAvailableMonths', getAvailableMonths())
 
